b681686c09c82c0196c168fc8204825c57e82fe7
Rahman115 <abdurrahmanariyansyah@gmail.com>
2021-09-12 01:38:08 +0700

    update

---
+++ code/iqfaucet/run.php
@@ -0,0 +1,125 @@
+<?php
+// open site https://rahman115.github.io/ListFaucetPay/
+// Klik IQFaucet
+
+
+system('clear');
+
+// aunt link
+$url = [
+	'cek' =>"https://iqfaucet.com/account.php",
+	'wd' => "https://iqfaucet.com/account.php?withdr=fp"
+];
+$user = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0";
+$cookie = "PHPSESSID=u3o02j6rt2ncuaegpunl6g0e25; refer=208087; _ccnsad_pop=500; bitmedia_fid=eyJmaWQiOiI1M2I4NzdhNWEyNDhiNDhiMTM4YWE3ZjZjYjY0Yzc2MCIsImZpZG5vdWEiOiI1MjA4ODI4ODczNTYwMjdmOGZlNGZiYTQyY2NkNGQ0NSJ9";
+
+$u = [
+'User-Agent: '.$user,
+'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+'Accept-Language: en-US,en;q=0.5',
+'Cookie: '.$cookie
+];
+
+function getCek($u, $url){
+	$ch=curl_init();
+  	curl_setopt_array($ch, array(
+	    CURLOPT_URL => $url,
+	    CURLOPT_RETURNTRANSFER => 1,
+	    //CURLOPT_CUSTOMREQUEST => 'GET',
+	    CURLOPT_FOLLOWLOCATION => 1,
+	    CURLOPT_HTTPHEADER => $u,
+	    CURLOPT_SSL_VERIFYPEER => 0,
+	    )
+    );
+    $result = curl_exec($ch);
+
+    $bl = explode(' DOGE</h2>', $result)[0];
+    $blc = explode('<h2>', $bl)[1];
+	$info = curl_getinfo($ch);
+
+	$val = floatval($blc);
+
+	// print_r(gettype($val));
+
+    $dt = [
+    	'bl' => $val,
+    	'http' => $info['http_code']
+    ];
+	
+	 print_r($result);
+    // print_r($info);
+    
+    curl_close($ch);
+    return $dt;
+
+}
+
+function getWithdrawl($u, $url){
+
+$ch=curl_init();
+  	curl_setopt_array($ch, array(
+	    CURLOPT_URL => $url,
+	    CURLOPT_RETURNTRANSFER => 1,
+	    //CURLOPT_CUSTOMREQUEST => 'GET',
+	    CURLOPT_FOLLOWLOCATION => 1,
+	    CURLOPT_HTTPHEADER => $u,
+	    CURLOPT_SSL_VERIFYPEER => 0,
+	    )
+    );
+    $result = curl_exec($ch);
+
+    $bl = explode(' DOGE was sent to your account', $result)[0];
+    $blc = explode('<div class="alert alert-success">', $bl)[1];
+	$info = curl_getinfo($ch);
+
+	print_r(gettype($blc));
+
+    $dt = [
+    	'bl' => $blc,
+    	'http' => $info['http_code']
+    ];
+	
+	echo "\nHas Send : \n";
+	print_r($dt);
+    // print_r($info);
+    
+    curl_close($ch);
+    // return $result;
+
+
+
+}
+
+while (true) {
+	system('clear');
+
+	$hs = getCek($u, $url['cek']);
+
+	if($hs['bl'] > 0.00000000) {
+		echo "\nBalance : ".$hs['bl'];
+		sleep(3);
+		echo "\nLakukan WD";
+		getWithdrawl($u, $url['wd']);
+		for($i = 60; $i > -1; $i--){
+			echo " \r";
+			echo "[{$i}] Reload";
+			sleep(1);
+		}
+	} else {
+		exit();
+		echo "\nWD panding";
+		for($i = 60; $i > -1; $i--){
+			echo " \r";
+			echo "[{$i}] Reload";
+			sleep(1);
+		}
+
+
+	} // end if else
+
+	echo "\n";
+
+} // end while 
+
+
+?>
